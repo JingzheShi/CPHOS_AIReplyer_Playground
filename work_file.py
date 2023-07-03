67b7bb1e15d1dfd2fe5dba1eab0b158c36a4a9e5
jz shi <shi-jz21@mails.tsinghua.edu.cn>
2023-07-03 12:05:58 +0800

    init03

--- work_file.py
+++ work_file.py
@@ -1,7 +1,14 @@
 def answer_user_question(user_wechat_nickname, user_question):
-    # TODO: fill in here to answer user's question.
-    # You can make new files in the /utils folder and import them here, to make this file cleaner.
-    pass
+    try:
+        # ---------------------------------- TODO ---------------------------------------
+        # TODO: fill in here to answer user's question.
+        # You can make new files in the /utils folder and import them here, to make this file cleaner.
+        
+        pass
 
-    import random # modify this
-    return 'test answer' + str(random.randint(114514,1919810)) # modify this
+        import random # modify this
+        return 'test answer' + str(random.randint(114514,1919810)) # modify this
+        # ---------------------------------- TODO ---------------------------------------
+    except Exception as e:
+        print(e)
+        return 'an Error of type ' + str(type(e)) + ' has occurred.'
